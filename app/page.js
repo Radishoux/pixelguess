c7642f3f384a35d10592fdc47c26fa8374ef0ffe
Rudy Quinternet <rudy.quinternet@gmail.com>
2023-10-19 19:02:08 +0200

    reset

---
+++ app/page.js
@@ -0,0 +1,31 @@
+import React from 'react'
+var axios = require('axios');
+
+var data = JSON.stringify({
+    "collection": "pics",
+    "database": "pics",
+    "dataSource": "photos",
+    "projection": {}
+});
+
+function PixlImg() {
+  return (
+    <img
+      id="pixlImg"
+      alt="Pixl logo"
+      src="https://mario.wiki.gallery/images/thumb/3/3e/MPSS_Mario.png/800px-MPSS_Mario.png"
+      width={500}
+      height={500}
+    />
+  )
+}
+
+export default function Home() {
+  return (
+    <div>
+      <PixlImg />
+      <div></div>
+    </div>
+  )
+}
+
